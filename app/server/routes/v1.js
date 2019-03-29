96d71a4ea7d02e958a1fddd0067bbf2622a0db31
thanhdip <qvnsq202@gmail.com>
2019-03-29 13:47:31 -0700

    get function for ingredients table

--- app/server/routes/v1.js
+++ app/server/routes/v1.js
@@ -5,10 +5,14 @@ const router 			= express.Router();
 
 const AdminController   = require("../controllers/admin.controller");
 const UserController   = require("../controllers/user.controller");
+const IngredientsController   = require("../controllers/ingredients.controller");
 
 router.get("/admins", AdminController.adminList);
 router.post("/createAdmin", AdminController.adminCreate);
 
 router.post("/createUser", UserController.userCreate);
+
+router.get("/allIngredients", IngredientsController.ingredientsList);
+router.get("/searchIngredients", IngredientsController.ingredientsSearch);
 
 module.exports = router;
