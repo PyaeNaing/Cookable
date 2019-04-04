1a2f06dd7960a99dc3d6447d1c659984a1d60b0e
pnaing@mail.sfsu.edu <pnaing@mail.sfsu.edu>
2019-04-04 11:47:10 -0700

    merge clean up

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import "./styles/App.css";
 import Login from "./components/login.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from './pageLayouts/mainPage.js';
-=======
-
-import React, { Component } from 'react';
-import AboutPage from './pageLayouts/aboutPage.js';
-import './App.css';
->>>>>>> 4dd0046ea50c24ad0f8eb5320d555281da29cd30
 
 class App extends Component {
   state = {
@@ -36,18 +29,11 @@ class App extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <div className="App">
         <Router>
           <Route path="/login" exact component={Login} />
         </Router>
         <MainPage />
-=======
-      <div>
-        <header>
-          <AboutPage />
-        </header>
->>>>>>> 4dd0046ea50c24ad0f8eb5320d555281da29cd30
       </div>
     );
   }
