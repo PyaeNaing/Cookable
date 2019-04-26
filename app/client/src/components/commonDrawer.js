127a348a6206e07610734063d8fb6b7a596b74df
shivamrai <shivam.rai.111@gmail.com>
2019-04-26 13:45:02 -0700

    pantry page updated with subpage events

--- app/client/src/components/commonDrawer.js
+++ app/client/src/components/commonDrawer.js
@@ -1,18 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import Avatar from "@material-ui/core/Avatar";
 import MenuList from '@material-ui/core/MenuList';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -20,7 +14,6 @@ import AccountBox2tone from '@material-ui/icons/AccountBoxTwoTone';
 import StarBorderTwoTone from '@material-ui/icons/StarBorderTwoTone';
 import PaletteTwoTone from '@material-ui/icons/PaletteTwoTone';
 import Pantry from "./pantry";
-
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -63,83 +56,83 @@ const styles = theme => ({
   }
 });
 
-function ClippedDrawer(props) {
-  const { classes } = props;
+class ClippedDrawer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      currentSubPage : 'pantry',
+      };
+    }
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      {/* <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Clipped drawer
-          </Typography>
-        </Toolbar>
-      </AppBar> */}
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.toolbar} />
-        <Avatar
-          alt="Profile picture"
-          src="/static/images/avatar/1.jpg"
-          className={classes.bigAvatar}
-        />
-        <br />
-        <br />
-        <br />
-        {/* <List>
-          {["Pantry", "My Recipes", "Favorite Recipes", "Profile"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List> */}
-        <MenuList>
-        <MenuItem className={classes.menuItem} onClick={() => this.handlePageChange('profile')}>
-          <ListItemIcon className={classes.icon}>
-            <AccountBox2tone />
-          </ListItemIcon>
-          <ListItemText classes={{ primary: classes.primary }} inset primary="Profile and Settings" />
-        </MenuItem>
-        <MenuItem className={classes.menuItem} onClick={() => this.handlePageChange('pantry')}>
-          <ListItemIcon className={classes.icon}>
-            <PaletteTwoTone />
-          </ListItemIcon>
-          <ListItemText classes={{ primary: classes.primary }} inset primary="Pantry" />
-        </MenuItem>
-        <MenuItem className={classes.menuItem} onClick={() => this.handlePageChange('favorites')}>
-          <ListItemIcon className={classes.icon}>
-            <StarBorderTwoTone />
-          </ListItemIcon>
-          <ListItemText classes={{ primary: classes.primary }} inset primary="Favorites" />
-        </MenuItem>
-        <MenuItem className={classes.menuItem} onClick={() => this.handlePageChange('myRecipes')}>
-          <ListItemIcon className={classes.icon}>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText classes={{ primary: classes.primary }} inset primary="My Recipes" />
-        </MenuItem>
-      </MenuList>
-        <Divider />
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Pantry />
-      </main>
-    </div>
-  );
-}
+    handleSubPageChange = (page) => {
+        this.setState({ currentSubPage: page });
+    }
+    
+    render() {
+
+      const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+              <CssBaseline />
+              {/* Clipped drawer toolbar removed as depreceated */}
+              <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+              >
+                <div className={classes.toolbar} />
+                <Avatar
+                  alt="Profile picture"
+                  src="/static/images/avatar/1.jpg"
+                  className={classes.bigAvatar}
+                />
+                <br />
+                <br />
+                <MenuList>
+                <MenuItem className={classes.menuItem} onClick={() => this.handleSubPageChange('profile')}>
+                  <ListItemIcon className={classes.icon}>
+                    <AccountBox2tone />
+                  </ListItemIcon>
+                  <ListItemText classes={{ primary: classes.primary }} inset primary="Profile and Settings" />
+                </MenuItem>
+                <MenuItem className={classes.menuItem} onClick={() => this.handleSubPageChange('pantry')}>
+                  <ListItemIcon className={classes.icon}>
+                    <PaletteTwoTone />
+                  </ListItemIcon>
+                  <ListItemText classes={{ primary: classes.primary }} inset primary="Pantry" />
+                </MenuItem>
+                <MenuItem className={classes.menuItem} onClick={() => this.handleSubPageChange('pantry')}>
+                  <ListItemIcon className={classes.icon}>
+                    <StarBorderTwoTone />
+                  </ListItemIcon>
+                  <ListItemText classes={{ primary: classes.primary }} inset primary="Favorites" />
+                </MenuItem>
+                <MenuItem className={classes.menuItem} onClick={() => this.handleSubPageChange('pantry')}>
+                  <ListItemIcon className={classes.icon}>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText classes={{ primary: classes.primary }} inset primary="My Recipes" />
+                </MenuItem>
+              </MenuList>
+                <Divider />
+              </Drawer>
+              <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Pantry />
+              </main>
+            </div>
+          );
+          
+    }
+  }
+
+
 
 ClippedDrawer.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
+    classes: PropTypes.object.isRequired
+  };
+  
 export default withStyles(styles)(ClippedDrawer);
