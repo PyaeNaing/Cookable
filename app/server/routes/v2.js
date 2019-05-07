02fbbad863ee7fce72e7a764f791f8113bb46250
thanhdip <21034756+thanhdip@users.noreply.github.com>
2019-05-06 20:29:56 -0700

    Added info return to protected test route

--- app/server/routes/v2.js
+++ app/server/routes/v2.js
@@ -23,11 +23,19 @@ router.post("/recipe/create", Recipe.createRecipe);
 router.post("/user/pantry", Recipe.searchRecipe);
 router.get("/searchByRecipe", Recipe.searchByRecipe);
 router.get("/searchByIngredient", Recipe.searchByIngredient);
-router.get('/protected', passport.authenticate('jwt', { session: false }), function(req, res) {
-    
-    res.json({ msg: 'Congrats! You are seeing this because you are authorized'});
-    
+router.get("/")
+router.get('/protected', passport.authenticate('jwt', { session: false }), function (req, res) {
+
+    console.log(req.user.userID);
+
+    res.json({
+        msg: 'Congrats! You are seeing this because you are authorized',
+        "userID": req.user.userID,
+        "username": req.user.username,
+        "emailAddress": req.user.emailAddress
     });
+
+});
 
 //  router.get("/test", Recipe.testSearch);
 
