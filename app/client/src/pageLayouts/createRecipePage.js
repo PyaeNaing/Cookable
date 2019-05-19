907e9baaf6adae34b57c9c224c17d74ce0310221
tonersrodriguez <trodriguez13983@yahoo.com>
2019-05-18 18:13:44 -0700

    got the createRecipePage to handle user input

--- app/client/src/pageLayouts/createRecipePage.js
+++ app/client/src/pageLayouts/createRecipePage.js
@@ -2,11 +2,8 @@ import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
@@ -15,6 +12,7 @@ import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 import FolderIcon from '@material-ui/icons/Folder';
 import MenuItem from '@material-ui/core/MenuItem';
+import axios from "axios";
 
 const styles = theme => ({
   main: {
@@ -53,6 +51,7 @@ const styles = theme => ({
   },
 });
 
+
 class CreateRecipePage extends Component{
   constructor(props){
     super(props); 
@@ -66,6 +65,14 @@ class CreateRecipePage extends Component{
       uploadImage: "", 
     };
   }; 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }; 
+
   render (){
     const {classes} = this.props; 
     return (
@@ -118,22 +125,26 @@ class CreateRecipePage extends Component{
             Remove
           </Button></div>
           </FormControl>
-		  <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="cuisine">Cuisine</InputLabel>
-            <Input id="cuisine" name="cuisine" />
-           <div>
-           <Select
+          <FormControl className={classes.formControl} required fullWidth>
+          <InputLabel htmlFor="cuisine">cuisine</InputLabel>
+          <Select
+            value={this.state.cuisine}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'cuisine',
+              id: 'cuisine',
+            }}
           >
             <MenuItem value="">
-              <em>Snack</em>
+            <em>Meal Type</em>
             </MenuItem>
-            <MenuItem value={10}>Breakfast</MenuItem>
-            <MenuItem value={20}>Brunch</MenuItem>
-            <MenuItem value={30}>Lunch</MenuItem>
-            <MenuItem value={30}>Dinner</MenuItem>
+            <MenuItem value={10}>Snack</MenuItem>
+            <MenuItem value={20}>Breakfast</MenuItem>
+            <MenuItem value={30}>Brunch</MenuItem>
+            <MenuItem value={40}>Lunch</MenuItem>
+            <MenuItem value={50}>Dinner</MenuItem>
           </Select>
-            </div>
-          </FormControl>		  
+        </FormControl>
 		  <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="uploadImage">Upload Image</InputLabel>
             <Input id="uploadImage" name="uploadImage" />
--- app/client/src/pageLayouts/mainPage.js
+++ app/client/src/pageLayouts/mainPage.js
@@ -12,7 +12,7 @@ class MainPage extends Component {
 		super(props);
 		this.state = {
 			// Change currentPage for testing only, change it back to 'recommendationsPage'
-			currentPage: 'recommendationsPage',
+			currentPage: 'createRecipePage',
 			profileSubpage: '',
 			loginRegisterSubpage: 'login',
 			searchResult: [],
