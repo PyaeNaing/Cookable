6bccf9817718477ba5cfa8313321320d0fb641e3
pnaing@mail.sfsu.edu <pnaing@mail.sfsu.edu>
2019-05-26 16:00:02 -0700

    Updates

---
+++ precache-manifest.afe549e7d744deefaeffc7e77cf80bba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "873e36ef3f9181625e2e5e240a236075",
+    "url": "/Cookable/static/media/cookable.873e36ef.gif"
+  },
+  {
+    "revision": "32c04f9dfde0546e2b11",
+    "url": "/Cookable/static/js/runtime~main.cd88d820.js"
+  },
+  {
+    "revision": "a85502868c15f155de60",
+    "url": "/Cookable/static/js/main.f3eefc11.chunk.js"
+  },
+  {
+    "revision": "27eaa6086093393555aa",
+    "url": "/Cookable/static/js/2.10b391f0.chunk.js"
+  },
+  {
+    "revision": "a85502868c15f155de60",
+    "url": "/Cookable/static/css/main.9c4b0144.chunk.css"
+  },
+  {
+    "revision": "27eaa6086093393555aa",
+    "url": "/Cookable/static/css/2.d54851e2.chunk.css"
+  },
+  {
+    "revision": "02a0365739132cb55899901b70421f60",
+    "url": "/Cookable/index.html"
+  }
+];
